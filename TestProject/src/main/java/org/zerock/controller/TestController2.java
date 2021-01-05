911eb701f2bfe80910e08c48b2b67ed9cf8e1bf4
lssoy66 <lssoy66@naver.com>
2021-01-05 12:36:12 +0900

    testMerge

--- TestProject/src/main/java/org/zerock/controller/TestController2.java
+++ TestProject/src/main/java/org/zerock/controller/TestController2.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TestController2 {
 	
-	// Branch & Merge Test - feature/2/.......
+	// Branch & Merge Test - feature/2/.........
 }
